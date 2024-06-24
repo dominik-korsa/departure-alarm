bc3ffee353a55bd29020d9991ff4b5a853d54030
dominik-korsa <29484605+dominik-korsa@users.noreply.github.com>
2024-06-24 13:52:07 +0200

    Change vibration effects again

--- app/src/main/java/eu/dkgl/departurealarm/AlarmType.kt
+++ app/src/main/java/eu/dkgl/departurealarm/AlarmType.kt
@@ -8,17 +8,27 @@ enum class AlarmType {
     Prepare {
         override val timeBeforeDeparture = 30.seconds
         override val vibrationEffect: VibrationEffect =
-            VibrationEffect.createWaveform(longArrayOf(0, 75, 50, 75, 50, 75, 50, 75, 500, 75, 50, 75, 50, 75, 50, 75), -1)
+            VibrationEffect.createWaveform(
+                longArrayOf(0, 75, 50, 75, 50, 75, 50, 75, 500, 75, 50, 75, 50, 75, 50, 75),
+                -1,
+            )
     },
     Whistle {
         override val timeBeforeDeparture = 10.seconds
         override val vibrationEffect: VibrationEffect =
-            VibrationEffect.createOneShot(1000, VibrationEffect.DEFAULT_AMPLITUDE)
+            VibrationEffect.createWaveform(
+                longArrayOf(100, 200, 100, 200, 100, 200, 100, 200, 100),
+                intArrayOf(255, 31, 255, 31, 255, 31, 255, 31, 255),
+                -1,
+            )
     },
     Departure {
         override val timeBeforeDeparture = 0.seconds
         override val vibrationEffect: VibrationEffect =
-            VibrationEffect.createWaveform(longArrayOf(0, 100, 200, 100), -1)
+            VibrationEffect.createWaveform(
+                longArrayOf(0, 100, 200, 100),
+                -1,
+            )
     };
 
     abstract val timeBeforeDeparture: Duration
