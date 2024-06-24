2b7c33d2f5ed8e7a1a1e50f1473be2422eb71514
dominik-korsa <29484605+dominik-korsa@users.noreply.github.com>
2024-06-24 13:23:22 +0200

    Vibrate upon alarm

--- app/src/main/java/eu/dkgl/departurealarm/receiver/AlarmReceiver.kt
+++ app/src/main/java/eu/dkgl/departurealarm/receiver/AlarmReceiver.kt
@@ -3,13 +3,40 @@ package eu.dkgl.departurealarm.receiver
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.os.Build
+import android.os.VibrationAttributes
+import android.os.VibrationEffect
+import android.os.Vibrator
+import android.os.VibratorManager
 import android.widget.Toast
 import eu.dkgl.departurealarm.AlarmType
+
 
 class AlarmReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context, intent: Intent) {
         val type = intent.getStringExtra(EXTRA_TYPE)!!.let { AlarmType.valueOf(it) }
+        vibrate(context)
         Toast.makeText(context, "ALARM of type $type went off!!", Toast.LENGTH_LONG).show()
+    }
+
+    private fun vibrate(context: Context) {
+        val vibrator = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            val vibratorManager = context.getSystemService(Context.VIBRATOR_MANAGER_SERVICE) as VibratorManager
+            vibratorManager.defaultVibrator
+        } else {
+            @Suppress("DEPRECATION")
+            context.getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
+        }
+
+        val effect = VibrationEffect.createOneShot(100, 255)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            vibrator.vibrate(
+                effect,
+                VibrationAttributes.createForUsage(VibrationAttributes.USAGE_ALARM),
+            )
+        } else {
+            vibrator.vibrate(effect)
+        }
     }
 
     companion object {
